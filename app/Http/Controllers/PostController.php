20b0d4816ede2f8163cbff3bfa3ab144b2f717b7
nicohe <h.nico@hotmail.com.ar>
2016-12-02 16:13:19 -0300

    PostListTest

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,13 @@ use App\Post;
 
 class PostController extends Controller
 {
+    function index()
+    {
+      $posts = Post::orderBy('created_at', 'DESC')->paginate();
+
+      return view('posts.index', compact('posts'));
+    }
+
     public function show(Post $post, $slug)
     {
       if ($post->slug != $slug){
